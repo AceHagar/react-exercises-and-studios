87d34f391f50e1fa011cc16a4c0d13454708c29b
AceHagar <andyhaag@gmail.com>
2023-08-09 22:48:12 -0500

    finished

---
+++ part3/exercises/exercise/src/components/Introduction.js
@@ -0,0 +1,12 @@
+export default function HobbyIntroduction (){
+    return (
+        <div>
+            <h2>What to Know about Sourdough</h2>
+            <ol>
+                <li>Naturally cultivate Yeast can live for Hundreds of Years</li>
+                <li>Longer fermentation yields a more gut-friendly Bread</li>
+                <li>Traditional Sourdough has three ingredients: Flour, salt, waterj</li>
+            </ol>
+        </div>
+    )
+}
